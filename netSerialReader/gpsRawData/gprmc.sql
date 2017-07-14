9cf1fc9965d1511d4c940c3a79efe42e56a98308
David Jakobsson <davejakob@gmail.com>
2017-07-14 15:00:35 +0200

    Added rawDatabase project
    project for rawDatabase including table

---
+++ netSerialReader/gpsRawData/gprmc.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[gprmc](
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[MessageType] [nvarchar](5) NULL,
+	[Data] [nvarchar](1000) NOT NULL,
+	[Device] [nvarchar](50) NOT NULL,
+	[Parsed] [bit] NOT NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON)
+)
+
+GO
+
+ALTER TABLE [dbo].[gprmc] ADD  DEFAULT ((0)) FOR [Parsed]
+GO
