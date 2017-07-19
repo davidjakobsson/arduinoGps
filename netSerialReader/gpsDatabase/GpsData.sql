d325ac23ecfe5ebe7483e669a279571edd757708
David Jakobsson <davejakob@gmail.com>
2017-07-19 13:43:35 +0200

    Added GpsDatabase project
    GpsData table

---
+++ netSerialReader/gpsDatabase/GpsData.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[GpsData](
+	[Id] [int] IDENTITY (1,1) NOT NULL PRIMARY KEY,
+	[Device] [nvarchar](50) NOT NULL,
+	[Latitude] [nvarchar](50) NULL,
+	[Longitude] [nvarchar](50) NULL,
+	[SpeedKnots] [decimal](18, 0) NULL,
+	[SpeedKph] [decimal](18, 0) NULL,
+	[UtcDateTime] [datetime2](7) NULL
+)
